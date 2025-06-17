923affafcb80f3594fad4540d3d1839e99a7f4ed
Klejdi <klejdijanora@gmail.com>
2025-06-17 20:33:09 +0200

    Updated launcher.sh

--- .config/rofi/launcher.sh
+++ .config/rofi/launcher.sh
@@ -4,6 +4,6 @@
 # author: Klejdi Janari (klejdiLOL)
 # OpenMandriva
 # https://forum.openmandriva.org/
-# Location: ~/.config/polybar/launcher.sh
+# Location: ~/.config/polybar/rofi/launcher.sh
 
 rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/rofi/rofi/launcher.rasi
